dbd841df9ef8d1f2e409405aea058cceef4d3b3e
Karlo Kus <kkus@neos.hr>
2025-10-02 13:39:19 +0200

    Section 4: Signals, services, modules

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,29 +1,35 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import type { InvestmentInput } from '../investment-input.model';
+import { Component, signal } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+//import type { InvestmentInput } from '../investment-input.model';
 
 @Component({
     selector: 'app-user-input',
-    standalone: true,
-    imports: [FormsModule],
     templateUrl: './user-input.component.html',
     styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-    enteredInitialInvestment = '0';
-    enteredAnnualInvestment = '0';
-    enteredExpectedReturn = '5';
-    enteredDuration = '10';
-    @Output() calculate = new EventEmitter<InvestmentInput>();
+    enteredInitialInvestment = signal('0');
+    enteredAnnualInvestment = signal('0');
+    enteredExpectedReturn = signal('5');
+    enteredDuration = signal('10');
+    //calculate = output<InvestmentInput>();
+
+    constructor(private readonly investmentService: InvestmentService) {}
 
     onSubmit() {
         const data = {
-            initialInvestment: +this.enteredInitialInvestment,
-            duration: +this.enteredDuration,
-            expectedReturn: +this.enteredExpectedReturn,
-            annualInvestment: +this.enteredAnnualInvestment,
+            initialInvestment: +this.enteredInitialInvestment(),
+            duration: +this.enteredDuration(),
+            expectedReturn: +this.enteredExpectedReturn(),
+            annualInvestment: +this.enteredAnnualInvestment(),
         };
 
-        this.calculate.emit(data);
+        //this.calculate.emit(data);
+        this.investmentService.calculateInvestmentResults(data);
+
+        this.enteredInitialInvestment.set('0');
+        this.enteredAnnualInvestment.set('0');
+        this.enteredExpectedReturn.set('5');
+        this.enteredDuration.set('10');
     }
 }
