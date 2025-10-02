9eee54910356be762768e10e3a88bc49937e8645
Karlo Kus <kkus@neos.hr>
2025-10-02 09:33:59 +0200

    Section 4: Project init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,8 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [HeaderComponent, UserComponent, TasksComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
-    users = DUMMY_USERS;
-    selectedUserId?: string;
-
-    get selectedUser() {
-        return this.users.find((user) => user.id === this.selectedUserId);
-    }
-
-    onSelectUser(id: string) {
-        this.selectedUserId = id;
-    }
-}
+export class AppComponent {}
--- src/app/header/header.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-header',
-    standalone: true,
-    templateUrl: './header.component.html', //template: '<h1>Hello World<h1>'
-    styleUrls: ['./header.component.css']
-})
-export class HeaderComponent {}
